d705605d5bd148e3802ea3996efc22371051f346
Venti1707 <nickohse3@gmail.com>
2025-05-08 22:17:01 +0800

    Create CSVs.py

---
+++ 02 - Bar Charts and Analyzing Data from CSVs/CSVs.py
@@ -0,0 +1,29 @@
+# https://www.youtube.com/watch?v=nKxLfUrkLE8
+
+import csv
+import numpy as np
+from collections import Counter
+from matplotlib import pyplot as plt
+
+with open("02 - Bar Charts and Analyzing Data from CSVs/Data.csv") as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    language_counter = Counter()
+    for row in csv_reader:
+        language_counter.update(row["LanguagesWorkedWith"].split(";"))
+
+languages = []
+users = []
+
+for item in language_counter.most_common():
+    languages.append(item[0])
+    users.append(item[1])
+
+languages.reverse()
+users.reverse()
+
+plt.barh(languages, users)
+plt.title("Most popular programming languages")
+plt.xlabel("Users")
+plt.style.use('fivethirtyeight')
+plt.savefig("C:/OtherProjects/Learning-MatPlotLib/02 - Bar Charts and Analyzing Data from CSVs/CSVs.png")
+plt.show()
