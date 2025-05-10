36701632eeed53ac2993e2382c26d6eaea093131
Venti1707 <nickohse3@gmail.com>
2025-05-10 16:56:24 +0800

    Update First Plots.py

--- 01 - Creating and Customizing Our First Plots/First Plots.py
+++ 01 - Creating and Customizing Our First Plots/First Plots.py
@@ -1,5 +1,4 @@
 # https://www.youtube.com/watch?v=UO98lJQ3QGI
-
 from matplotlib import pyplot as plt
 
 ages_x = [18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55]
@@ -19,5 +18,4 @@ plt.title("Median salary (USD) by age")
 plt.legend()
 plt.style.use('fivethirtyeight')
 plt.savefig("C:/OtherProjects/Learning-MatPlotLib/01 - Creating and Customizing Our First Plots/First Plots.png")
-
 plt.show()
