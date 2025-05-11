a8b7b990f1759b326622e1295b651e31922e211e
Venti1707 <nickohse3@gmail.com>
2025-05-11 15:51:43 +0800

    Update Snippets.py

--- Snippets.py
+++ Snippets.py
@@ -24,3 +24,38 @@ users = [59219, 55466, 47544, 36443, 35917, 31991, 27097, 23030, 20524, 18523, 1
 dev1 = [8, 6, 5, 5, 4, 2, 1, 1, 0]
 dev2 = [0, 1, 2, 2, 2, 4, 4, 4, 4]
 dev3 = [0, 1, 1, 1, 2, 2, 3, 3, 4]
+
+# For real-time data
+import csv
+import random
+import time
+
+x_value = 0
+total_1 = 1000
+total_2 = 1000
+
+fieldnames = ["x_value", "total_1", "total_2"]
+
+with open('data.csv', 'w') as csv_file:
+    csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+    csv_writer.writeheader()
+
+while True:
+
+    with open('data.csv', 'a') as csv_file:
+        csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+
+        info = {
+            "x_value": x_value,
+            "total_1": total_1,
+            "total_2": total_2
+        }
+
+        csv_writer.writerow(info)
+        print(x_value, total_1, total_2)
+
+        x_value += 1
+        total_1 = total_1 + random.randint(-6, 8)
+        total_2 = total_2 + random.randint(-5, 6)
+
+    time.sleep(1)
